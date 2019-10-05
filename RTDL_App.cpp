eeb4d718104be784729c4376dfbc586b6ca46f29
iamgiuly <lilyblackeyes@hotmail.it>
2019-10-05 17:36:10 +0200

    Update RTDL_App.cpp

--- RTDL_App.cpp
+++ RTDL_App.cpp
@@ -127,7 +127,7 @@ int main() {
 	clock_nanosleep(CLOCK_MONOTONIC, 0, &dt, NULL); 
 	pthread_create(&tid2, NULL, consumatore, &id2);
 	clock_nanosleep(CLOCK_MONOTONIC, 0, &dt, NULL);
-	pthread_create(&tid2, NULL, consumatore, &id3);
+	pthread_create(&tid3, NULL, consumatore, &id3);
 	pthread_join(tid1, NULL);
 	pthread_join(tid2, NULL);
 	pthread_join(tid3, NULL);
